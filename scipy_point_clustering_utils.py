116856e37a702dafdaec99d751b1abe79198a780
Henry Walshaw <henry.walshaw@gmail.com>
2016-03-18 22:53:34 +1100

    Commit the plugin framework
    The plugin as generated by the QGIS plugin builder tool.
    
    Signed-off-by: Henry Walshaw <henry.walshaw@gmail.com>

---
+++ scipy_point_clustering_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+/***************************************************************************
+ DifferentialPrivacy
+                                 A QGIS plugin
+ Methods for anonymizing data for public distribution
+                              -------------------
+        begin                : 2015-11-02
+        copyright            : (C) 2015 by Henry Walshaw
+        email                : henry.walshaw@spatialvision.com.au
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'Henry Walshaw'
+__date__ = '2016-03-18'
+__copyright__ = '(C) 2015 by Henry Walshaw'
+
+import os.path
+
+from PyQt4.QtGui import QIcon
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+
+class ScipyPointClusteringUtils(object):
+
+    @staticmethod
+    def getIcon():
+        return QIcon(os.path.join(
+            os.path.dirname(__file__),
+            'icons',
+            'seagull.png'
+        ))
